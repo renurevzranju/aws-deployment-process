1e7ecaed0f0fd22f608144fa8dc1ac87149cfcf0
Revathi <revathimeenashi@gmail.com>
2023-07-30 18:37:57 +0530

    api load issue fix

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://udagram-api-revz-dev.eba-p2pv3b6n.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
